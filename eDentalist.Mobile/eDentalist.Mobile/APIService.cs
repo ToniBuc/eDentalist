5903da48e48af2f1799f95708a215022e0b97db2
ToniBuc <59138588+ToniBuc@users.noreply.github.com>
2021-03-18 17:33:32 +0100

    Added Mobile project to solution and mobile login page.

---
+++ eDentalist.Mobile/eDentalist.Mobile/APIService.cs
@@ -0,0 +1,104 @@
+﻿using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using eDentalist.Model;
+using System.Threading.Tasks;
+
+
+namespace eDentalist.Mobile
+{
+    public class APIService
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string Role { get; set; }
+        public static int UserID { get; set; }
+
+        private string _route = null;
+
+#if DEBUG
+        private string _apiUrl = "http://localhost:44311/api";
+#endif
+#if RELEASE
+        private string _apiUrl = "https://mywebsite.com/api";
+#endif
+        public APIService(string route)
+        {
+            _route = route;
+        }
+
+        public async Task<T> Get<T>(object search)
+        {
+
+            var url = $"{_apiUrl}/{_route}";
+
+            if (search != null)
+            {
+                url += "?";
+                url += await search.ToQueryString();
+            }
+
+            var result = await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+
+            return result;
+        }
+
+        public async Task<T> GetStaff<T>(object search)
+        {
+
+            var url = $"{_apiUrl}/{_route}/GetStaff";
+
+            if (search != null)
+            {
+                url += "?";
+                url += await search.ToQueryString();
+            }
+
+            var result = await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+
+            return result;
+        }
+
+        public async Task<T> GetById<T>(object id)
+        {
+
+            var url = $"{_apiUrl}/{_route}/{id}";
+
+            var result = await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+
+            return result;
+        }
+
+        public async Task<T> Insert<T>(object request)
+        {
+
+            var url = $"{_apiUrl}/{_route}";
+
+            var result = await url.WithBasicAuth(Username, Password).PostJsonAsync(request).ReceiveJson<T>();
+
+            return result;
+        }
+
+        public async Task<T> Update<T>(object id, object request)
+        {
+
+            var url = $"{_apiUrl}/{_route}/{id}";
+
+            var result = await url.WithBasicAuth(Username, Password).PutJsonAsync(request).ReceiveJson<T>();
+
+            return result;
+        }
+
+        public async Task<T> Login<T>(object request)
+        {
+
+            var url = $"{_apiUrl}/{_route}";
+
+            var result = await url.WithBasicAuth(Username, Password).PostJsonAsync(request).ReceiveJson<T>();
+
+            return result;
+        }
+    }
+}
---
+++ eDentalist.Mobile/eDentalist.Mobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,66 @@
+﻿using eDentalist.Mobile.Views;
+using eDentalist.Model.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace eDentalist.Mobile.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private readonly APIService _service = new APIService("User/Login");
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await Login());
+        }
+
+        string _username = string.Empty;
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty(ref _username, value); }
+        }
+
+        string _password = string.Empty;
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public ICommand LoginCommand { get; set; }
+
+        async Task Login()
+        {
+            IsBusy = true;
+            APIService.Username = Username;
+            APIService.Password = Password;
+            var login = new UserLoginRequest()
+            {
+                Username = APIService.Username,
+                Password = APIService.Password
+            };
+            try
+            {
+                //await _service.Get<dynamic>(null);
+
+                var userLogin = await _service.Login<Model.User>(login);
+
+                APIService.UserID = userLogin.UserID;
+                APIService.Role = userLogin.UserRole.Name;
+
+                Application.Current.MainPage = new MainPage();
+            }
+            catch (Exception)
+            {
+                IsBusy = false;
+                Username = string.Empty;
+                Password = string.Empty;
+                await Application.Current.MainPage.DisplayAlert("Error", "You have entered an incorrect username or password!", "OK");
+            }
+        }
+    }
+}
