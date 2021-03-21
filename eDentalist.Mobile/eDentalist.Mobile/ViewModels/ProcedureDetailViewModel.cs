589c2575b40d07de72f5357942b19c7d9e1d05be
ToniBuc <59138588+ToniBuc@users.noreply.github.com>
2021-03-21 17:03:31 +0100

    Added ProcedureDetailPage, still WIP

---
+++ eDentalist.Mobile/eDentalist.Mobile/ViewModels/ProcedureDetailViewModel.cs
@@ -0,0 +1,50 @@
+﻿using eDentalist.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace eDentalist.Mobile.ViewModels
+{
+    public class ProcedureDetailViewModel : BaseViewModel
+    {
+        private readonly APIService _procedureService = new APIService("Procedure");
+        public int ? ProcedureID { get; set; }
+        public ProcedureDetailViewModel()
+        {
+            InitCommand = new Command(async () => await Init());
+        }
+
+        //public ObservableCollection<Procedure> Procedure { get; set; } = new ObservableCollection<Procedure>();
+        public Procedure Procedure { get; set; }
+        public ICommand InitCommand { get; set; }
+
+        private string _name = string.Empty;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+        private string _description = string.Empty;
+        public string Description
+        {
+            get { return _description; }
+            set { SetProperty(ref _description, value); }
+        }
+
+        public async Task Init()
+        {
+            //var proc = await _procedureService.Get<IEnumerable<Model.Procedure>>(null);
+            Procedure = await _procedureService.GetById<Model.Procedure>(ProcedureID);
+
+            if (Procedure != null)
+            {
+                Name = Procedure.Name;
+                Description = Procedure.Description;
+            }
+        }
+    }
+}
--- eDentalist.Mobile/eDentalist.Mobile/Views/ProcedureDetailPage.xaml.cs
+++ eDentalist.Mobile/eDentalist.Mobile/Views/ProcedureDetailPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using eDentalist.Mobile.ViewModels;
-using eDentalist.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +11,22 @@ using Xamarin.Forms.Xaml;
 namespace eDentalist.Mobile.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ProceduresPage : ContentPage
+    public partial class ProcedureDetailPage : ContentPage
     {
-        private ProceduresViewModel model = null;
-        public ProceduresPage()
+        private ProcedureDetailViewModel model = null;
+        public ProcedureDetailPage(int ? id)
         {
             InitializeComponent();
-            BindingContext = model = new ProceduresViewModel();
+            BindingContext = model = new ProcedureDetailViewModel()
+            {
+                ProcedureID = id
+            };
         }
 
         protected async override void OnAppearing()
         {
             base.OnAppearing();
             await model.Init();
-        }
-        async void OnTapGestureRecognizerTapped(object sender, EventArgs e)
-        {
-            var x = (((TappedEventArgs)e).Parameter) as int?;
-            await Navigation.PushAsync(new ProcedureDetailPage(x));
         }
     }
 }
